bcf6f62f77efa1b78e5fae26aafbc9e3c7f8c9b2
JeongHwi_Ra <inyun2uspam@gmail.com>
2014-05-28 09:59:00 +0900

    upload

---
+++ WebApplication1/JavaScript1.js
@@ -0,0 +1,16 @@
+﻿$(document).ready(function () {
+    var array = [
+        { name: 'hanbit media', link: "http://hanb.co.kr" },
+        { name: 'NAver', link: "http://www.naver.com" },
+        { name: 'paran', link: "http://www.paran.com" }
+    ];
+    $.each(array, function (index, item) {
+        var output = '';
+        output += '<a href="' + item.link + '">';
+        output += '<h1>' + item.name + '</h1>';
+        output += '</a>';
+        document.body.innerHTML += output;
+    });
+
+}
+);
