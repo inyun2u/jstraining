946d8258661522b926a7acdd874ac302ddb6c82b
inyun2u <inyun2uspam@gmail.com>
2014-05-28 10:37:56 +0900

    fghjfghjh

--- WebApplication1/JavaScript1.js
+++ WebApplication1/JavaScript1.js
@@ -1,16 +1,5 @@
 ﻿$(document).ready(function () {
-    var array = [
-        { name: 'hanbit media', link: "http://hanb.co.kr" },
-        { name: 'NAver', link: "http://www.naver.com" },
-        { name: 'paran', link: "http://www.paran.com" }
-    ];
-    $.each(array, function (index, item) {
-        var output = '';
-        output += '<a href="' + item.link + '">';
-        output += '<h1>' + item.name + '</h1>';
-        output += '</a>';
-        document.body.innerHTML += output;
-    });
 
+    $('h1').addClass
 }
 );
